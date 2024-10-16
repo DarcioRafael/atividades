d59fac0ed7705d0283f80e0459aa90c56eb9b6bd
DarcioRafael <liminhadarcioo@gmail.com>
2024-10-16 19:11:24 -0300

    Add files via upload

---
+++ atividades.py
@@ -0,0 +1,207 @@
+
+#exercicio 1
+# numero1 = float(input(' digite 1 numero1: '))
+# numero2 = float(input(' digite 2 numero: '))
+
+# resultado = numero1+numero2
+
+# print(' O Resultado de ', numero1, '+',numero2, '=', resultado)
+
+#exercicio 2
+# numero1 = float(input(' digite 1 numero1: '))
+# numero2 = float(input(' digite 2 numero: '))
+
+# resultado = numero1-numero2
+
+# print(' O Resultado de ', numero1, '-',numero2, '=', resultado)
+
+#exercicio 3
+# numero1 = float(input(' digite 1 numero1: '))
+# numero2 = float(input(' digite 2 numero: '))
+
+# resultado = numero1*numero2
+
+# print(' O Resultado de ', numero1, '*',numero2, '=', resultado)
+
+#exercicio 4
+# numero1 = float(input(' digite 1 numero1: '))
+# numero2 = float(input(' digite 2 numero: '))
+
+# resultado = numero1/numero2
+
+# print(' O Resultado de ', numero1, '/',numero2, '=', resultado)
+
+#exercicio 5
+# numero1 = float(input(' digite 1 numero1: '))
+# numero2 = float(input(' digite 2 numero: '))
+
+# resultado = numero1%numero2
+
+# print(' O Resultado de ', numero1, '%',numero2, '=', resultado)
+
+#exercicio 6
+# numero1 = float(input(' digite 1 numero1: '))
+# numero2 = float(input(' digite 2 numero: '))
+
+# resultado = numero1**numero2
+
+# print(' O Resultado de ', numero1, '**',numero2, '=', resultado)
+
+#exercicio 7
+# numero1 = float(input(' digite 1 numero1: '))
+# numero2 = float(input(' digite 2 numero: '))
+# numero3 = float(input(' digite 3 numero: '))
+
+# resultado = (numero1+numero2+numero3)/3
+
+# print(' O Resultado de ', numero1, '+',numero2, '+' ,numero2, '=', resultado)
+
+#exercicio 8
+# temperatura = float(input(' digite uma temperatura: '))
+# resultado = 9* temperatura / 5 + 32
+# print(f' A temperatura em fahrenheit é: {resultado}')
+
+#exercicio 9
+# moeda = float(input(' digite um valor em R$: '))
+# conversao = moeda * 0.17822
+# print(f' O valor de {moeda}, R$ em dolar é: {conversao} USD ')
+
+#exercicio 10
+# largura = float(input(' digite a largura: '))
+# altura = float(input(' digite a altura: '))
+# area = largura * altura
+# print(f' A aréa de um retangulo com largura: {largura} e altura: {altura} é: {area}')
+
+#exercicio 11
+# lado_de_quadrado = float(input(' Digite o tamanho do lado: '))
+# perimetro = lado_de_quadrado * 4
+# print(f' o perimetro de um quadrado com o lado: {lado_de_quadrado} é igual a: {perimetro} ')
+
+#exercicio 12
+# base = float(input('digite o tamanho da base: '))
+# altura = float(input('digite a altura: '))
+# area = base * altura / 2
+# print(f' a area de um triangulo com base de: {base} e altura de: {altura} é igual a: {area}')
+
+#exercicio 13
+# raio = float(input(' digite o raio do circulo: '))
+# area = 3.14 * raio ** 2
+# print(f' a area de um circulo com raio: {raio} é igual: {area}') 
+
+#exercicio 14
+# metros =  float(input(' Digite a quantidade de metros: '))
+# conversao = metros * 100
+# print(f' a conversão de: {metros}, M em centimetros é: {conversao}, CM')
+
+#exercicio 15
+# horas = float(input(' digite a quantidade de horas trabalhadas: '))
+# valor_hora = float(input(' digite o valor pago por hora: '))
+# salario = horas * valor_hora
+# print(f' o salario de um funcionario que trabalha: {horas}  horas é igual a: {salario} R$')
+
+#exercicio 16
+# preco = float(input('qual o preço do produlto ?: '))
+# desconto = float(input(' qual o valor do desconto ?: '))
+# desconto_percentual = (preco- desconto) / preco * 100
+# print(f' o valor do produlto com desconto é: {desconto_percentual}')
+
+#exercicio 17
+# distancia = float(input('digite a distancia percorrida: '))
+# tempo = float(input(' digite o tempo de viagem: '))
+# media = distancia / tempo
+# print(f' a velocidade media foi de: {media} KM, por horas:')
+
+# #exercicio 18
+# idade = int(input(' qual a sua idade ? : '))
+# dias = idade * 365
+# print(f' Voce tem: {dias}, dias de vida')
+
+#exercicio 19
+
+# segundos = 24 * 60 * 60
+# print(f'um dia contem {segundos}, segundos')
+
+# exercicio 20
+
+# peso = float(input(' qual seu peso ?: '))
+# altura = float(input(' qual a sua altura ?: '))
+# imc = peso / altura ** 2
+# print(f' o seu imc é equivalente a: {imc}')
+
+#exercicio 21
+
+# numero1 = float(input(' digite um numero: '))
+# numero2 = float(input(' digite o segundo numero: '))
+# diferenca = numero1 - numero2
+# print(f' a diferença entre {numero1} e {numero2 } é = {diferenca}')
+
+#exercicio 22
+# numero1 = int(input(' digite um numero inteiro: '))
+# numero2 = int(input(' digite outro numero inteiro: '))
+# divisao = numero1 // numero2
+# print(f' a divisao inteira de {numero1} e {numero2} é igual a: {divisao}')
+
+#exercicio 23
+# numero = float(input(' digite um numero: '))
+# valor_absoluto = (numero ** 2) ** 0.5
+# print(f' o valor absoluto de : {numero} é = {valor_absoluto}')
+
+#exerciccio 24
+
+# velocidade = float(input(' digite uma velocidade: '))
+# velocidade_ms = velocidade / 3.6
+# print(f' a velocidade : {velocidade}KM/H  é: {velocidade_ms: .2F}  M/S')
+
+#exercicio 25
+
+# a = float(input(' digite o valor de A: '))
+# b = float(input(' digite o valor de B: '))
+# c = float(input(' digite o valor de C: '))
+# delta = b ** 2 - 4 * a * c
+
+# print(f' o valor de delta é:{delta} ')
+
+# x1 = (-b- delta**0.5) / (2*a)
+# x2 = (-b+ delta**0.5) / (2*a)
+
+# print(f' o valor de X1 é: {x1}')
+# print(f' o valor de X2 é: {x2}')
+
+#exercicio 26
+
+# produlto1 = float(input(' digite o valor do primeiro produlto: '))
+# produlto2 = float(input(' digite o valor do segundo produlto: '))
+# produlto3 = float(input(' digite o valor do terceiro produlto: '))
+# valor_total = produlto1 + produlto2 + produlto3
+
+# print(f' o valor total da compra é: {valor_total: .2f} R$')
+
+#exercicio 27
+
+# dia = int(input(' digite um dia: '))
+# conversao_semanas = dia // 7
+# conversao_dias = dia % 7
+# print(f'{dia} dias é igual: {conversao_semanas} semanas e {conversao_dias} dias')
+
+#exercicio 28
+
+# valor = float(input(' digite o valor da compra: '))
+# desconto = valor * 5 / 100
+# desconto2 = valor * 10 / 100
+# if valor >= 100:
+#     print(f' o valor é {valor - desconto: .2f}')
+# elif valor >= 500:
+#          print(f' o valor é: {valor - desconto2: .2f}')
+
+#exercicio 29
+
+# numero1 = float(input(' digite o primeiro numero: '))
+# numero2 = float(input(' digite o segundo numero: '))
+# divisao = numero1 / numero2
+# print(f' a divisao de: {numero1} e {numero2} é: {divisao: .2f}')
+
+
+
+
+
+
